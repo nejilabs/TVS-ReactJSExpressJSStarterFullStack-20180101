51a16887ae89ea3e9f3095166c0bf9863ddeab41
nellyXinwei <nelsonalbajr@yahoo.com>
2021-01-12 15:41:54 +0800

    1102 Create React App && 1625 Add Customer COmponent

---
+++ client/src/components/customers/Customers.js
@@ -0,0 +1,22 @@
+//START: IMPORTS
+import React, {Component} from 'react';
+import './customers.css';
+//END: IMPORTS
+
+//START: CLASS
+class Customers extends Component{
+  //start: Render Method
+  render(){
+    return (
+      <div>
+        <h2>Customers</h2>
+      </div>
+    )
+  };
+  //end: Render Method
+}
+//START: CLASS
+
+//START: EXPORTS
+export default Customers;
+//END: EXPORTS
